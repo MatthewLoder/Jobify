a243ab6a4babb0710943073fb47e9e3dee7acf95
Matthew Loder <matthewloder@Matthews-MacBook-Pro.local>
2024-07-06 20:51:47 -0400

    started validation middleware

---
+++ controllers/jobController.js
@@ -0,0 +1,35 @@
+import Job from "../models/jobModel.js";
+import { NotFoundError } from "../errors/customErrors.js";
+
+import { StatusCodes } from "http-status-codes";
+
+export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
+  res.status(StatusCodes.OK).json({ jobs });
+};
+
+export const createJob = async (req, res) => {
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
+
+export const getJob = async (req, res) => {
+  const { id } = req.params;
+  const job = await Job.findById(id);
+  if (!job) throw new NotFoundError(`no job with id ${id}`);
+  res.status(StatusCodes.OK).json({ job });
+};
+
+export const updateJob = async (req, res) => {
+  const { id } = req.params;
+  const updatedJob = await Job.findOneAndUpdate(id, req.body, { new: true });
+  if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
+  res.status(StatusCodes.OK).json({ msg: "job modified", updatedJob });
+};
+
+export const deleteJob = async (req, res) => {
+  const { id } = req.params;
+  const removeJob = await Job.findByIdAndDelete(id);
+  if (!removeJob) throw new NotFoundError(`no job with id ${id}`);
+  res.status(StatusCodes.OK).json({ msg: "job deleted", removeJob });
+};
