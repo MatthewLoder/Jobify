d264efa8bacf9806facfd698d3dfd51bd5d184c7
Matthew Loder <matthewloder@Matthews-MBP.cgocable.net>
2024-07-21 22:41:56 -0400

    finished validation

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,4 @@
 import Job from "../models/jobModel.js";
-import { NotFoundError } from "../errors/customErrors.js";
 
 import { StatusCodes } from "http-status-codes";
 
@@ -16,20 +15,17 @@ export const createJob = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-  if (!job) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {
   const { id } = req.params;
   const updatedJob = await Job.findOneAndUpdate(id, req.body, { new: true });
-  if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ msg: "job modified", updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const removeJob = await Job.findByIdAndDelete(id);
-  if (!removeJob) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ msg: "job deleted", removeJob });
 };
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
-import { validateTest } from "./middleware/validationMiddleware.js";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
@@ -18,11 +17,6 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
-
-app.post("/api/v1/test", validateTest, (req, res) => {
-  const { name } = req.body;
-  res.json({ message: `hello ${name}` });
-});
 
 app.use("/api/v1/jobs", jobRouter);
 
